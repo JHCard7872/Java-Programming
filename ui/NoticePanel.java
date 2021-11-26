b2f7f5770966589c12dfe183497fa1b74a18e3cc
joon-hyuk848 <74858961+joon-hyuk848@users.noreply.github.com>
2021-11-26 10:51:12 +0900

    Add files via upload

--- ui/NoticePanel.java
+++ ui/NoticePanel.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Stack;
 import java.util.Vector;
 
 
@@ -22,12 +23,14 @@ public class NoticePanel extends BasicPanel {
 	private BackButton GOtoBack = new BackButton();
 	private GoToBackPanel backbutton;
 	
-	private Vector<Vector> LimitCate = new Vector<Vector>(); // 여기에 초과한 카테고리 저장
+	private Vector<Vector<String>> LimitCate = new Vector<Vector<String>>(); // 여기에 초과한 카테고리 저장
 	
 	private JButton[] CateName;
 	
+	private JFrame frame;
 	public NoticePanel(JFrame frame) {
 		super();
+		this.frame = frame;
 		backbutton = new GoToBackPanel(frame); // BasicPanel에 있는 buttonclick
 		NorthPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 30)); // 왼쪽정렬, 수직거리 30
 		NorthPanel.add(Line);
@@ -81,11 +84,30 @@ public class NoticePanel extends BasicPanel {
 		// =========================================================================
 		
 		for(int i=0; i<CateName.length; i++) {
-			//CateName[i] = new JButton(LimitCate.toArray);
+			Vector<String> forName = (Vector<String>) LimitCate.toArray()[i];
+			CateName[i] = new JButton((String)forName.toArray()[0]);
 			CateName[i].setBackground(Color.BLACK);
 			CateName[i].setForeground(Color.WHITE);
 			CateName[i].setFont(new Font("맑은 고딕", Font.BOLD, 18));
-			CateName[i].addActionListener(buttonclick);
+			CateName[i].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JButton b = (JButton)e.getSource();
+					String CommaLimit = ""; // 여기에 콤마 포함 저장 문자열 
+					String expenseMoney = "";
+					Stack<Character> ExpenseToComma = addComma((String)forName.toArray()[2]); // 스택에 저장(addComma함수에 string 금액 넣어줘야함)
+					Stack<Character> LimitToComma = addComma((String)forName.toArray()[1]); // 스택에 저장(addComma함수에 string 금액 넣어줘야함)
+					while(!LimitToComma.isEmpty()) { // pop
+						CommaLimit += LimitToComma.pop();
+					}
+					while(!ExpenseToComma.isEmpty()) {
+						expenseMoney += ExpenseToComma.pop();
+					}
+					JOptionPane.showMessageDialog(null, "카테고리: "
+							+ b.getText()  + "\n" + "지출한도: " + 
+							CommaLimit + "원\n" + "초과금액: " + 
+							expenseMoney + "원", b.getText(), JOptionPane.DEFAULT_OPTION);
+				}
+			});
 			CenterPanel.add(CateName[i]); // 여기에 버튼들 GridLayout으로 추가
 		}
 		
@@ -101,5 +123,18 @@ public class NoticePanel extends BasicPanel {
 			frame.setContentPane(new MainPanel(frame));
 			frame.setVisible(true);
 		}
+	}
+	
+	private Stack<Character> addComma(String Money) {  					// 돈 콤마 추가해주는 함수
+		Money = Money.replaceAll(",","");
+		Money=Money.replaceAll("원","");
+		Stack<Character> ShowMoney = new Stack<Character>();
+		for(int i=0; i<Money.length(); i++) {
+			if(i%3==0 && i!= 0) {
+				ShowMoney.add(',');
+			}
+			ShowMoney.push(Money.charAt(Money.length()-1  - i));
+		}
+		return ShowMoney;
 	}
 }
